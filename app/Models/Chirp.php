bdb5f19733e7bacaf2fb74e7a1d2d31274146556
Yusuf Bouzekri <yusufbouzekri@gmail.com>
2023-10-26 07:37:21 +0100

    Add chirp editting functionality

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
@@ -11,7 +12,7 @@ class Chirp extends Model
 
     protected $fillable = ['message'];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
--- resources/views/livewire/chirps/list.blade.php
+++ resources/views/livewire/chirps/list.blade.php
@@ -8,23 +8,38 @@ use Livewire\Attributes\On;
 new class extends Component {
     public Collection $chirps;
 
+    public ?Chirp $editing = null;
+
     public function mount(): void
     {
         $this->getChirps();
     }
+
     #[On('chirp-created')]
-    public function getChirps()
+    #[On('chirp-updated')]
+    public function getChirps(): void
     {
+        $this->editing = null;
         $this->chirps = Chirp::with('user')
             ->latest()
             ->get();
     }
+
+    #[On('chirp-edit-cancelled')]
+    public function resetEdittingChirp()
+    {
+        $this->editing = null;
+    }
+
+    public function edit(Chirp $chirp): void
+    {
+        $this->editing = $chirp;
+    }
 }; ?>
 
 <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
-
-    @foreach ($chirps as $chirp)
-        <div class="p-6 flex space-x-2" wire:key="{{ $chirp->id }}">
+    @foreach ($chirps->sortByDesc('created_at') as $chirp)
+        <div wire:key='{{ $chirp->id }}' class="p-6 flex space-x-2">
 
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none"
                 viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -44,12 +59,50 @@ new class extends Component {
 
                         <small
                             class="ml-2 text-sm text-gray-600">{{ $chirp->created_at->format('j M Y, g:i a') }}</small>
+                        @unless ($chirp->created_at->eq($chirp->updated_at))
+                            <small class="text-sm text-gray-600"> &middot; {{ __('edited on') }}
+                                {{ $chirp->updated_at->format('j M Y, g:i a') }}`</small>
+                        @endunless
 
                     </div>
+                    @if ($chirp->user->is(auth()->user()))
+                        <x-dropdown>
 
+                            <x-slot name="trigger">
+
+                                <button>
+
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400"
+                                        viewBox="0 0 20 20" fill="currentColor">
+
+                                        <path
+                                            d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+
+                                    </svg>
+
+                                </button>
+
+                            </x-slot>
+
+                            <x-slot name="content">
+
+                                <x-dropdown-link wire:click="edit({{ $chirp }})">
+
+                                    {{ __('Edit') }}
+
+                                </x-dropdown-link>
+
+                            </x-slot>
+
+                        </x-dropdown>
+                    @endif
                 </div>
 
-                <p class="mt-4 text-lg text-gray-900">{{ $chirp->message }}</p>
+                @if ($chirp->is($editing))
+                    <livewire:chirps.edit :chirp="$chirp" />
+                @else
+                    <p class="mt-4 text-lg text-gray-900">{{ $chirp->message }}</p>
+                @endif
 
             </div>
 
